66222d6a1fe2c7839275d8c9553880a3f4af6616
benoit destresse <benoit.destresse@gmail.com>
2019-07-29 20:39:33 +0800

    long word

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+  def new
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = params[:word].upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
